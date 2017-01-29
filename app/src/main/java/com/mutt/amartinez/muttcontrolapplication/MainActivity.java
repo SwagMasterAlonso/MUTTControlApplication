89dc8ca0a93d3d086a01f4ebf6f4087ffc0268df
amartinez <almartnz@gmail.com>
2017-01-29 16:54:15 -0500

    Initial commit

---
+++ app/src/main/java/com/mutt/amartinez/muttcontrolapplication/MainActivity.java
@@ -0,0 +1,244 @@
+package com.mutt.amartinez.muttcontrolapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.bluetooth.*;
+import android.content.Intent;
+import java.util.Set;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BluetoothDevice mmDevice = null;
+
+    BluetoothSocket mmSocket;
+
+    final byte delimiter = 33;
+
+    int readBufferPosition = 0;
+
+
+    public void sendBtMsg(String msg2send) {
+        //UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
+        UUID uuid = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee"); //Standard SerialPortService ID
+        try {
+
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+            if (!mmSocket.isConnected()) {
+                mmSocket.connect();
+            }
+
+            String msg = msg2send;
+            //msg += "\n";
+            OutputStream mmOutputStream = mmSocket.getOutputStream();
+            mmOutputStream.write(msg.getBytes());
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        int REQUEST_ENABLE_BT = 0;
+
+
+        final Button fButton = (Button) findViewById(R.id.fButton);
+        final Button bButton = (Button) findViewById(R.id.bButton);
+        final Button lButton = (Button) findViewById(R.id.lButton);
+        final Button rButton = (Button) findViewById(R.id.rButton);
+
+
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice device : pairedDevices)
+            {
+                if(device.getName().equals("mutt")) //Note, you will need to change this to match the name of your device
+                {
+                    Log.e("Aquarium",device.getName());
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        final Handler handler = new Handler();
+
+
+        final class workerThread implements Runnable {
+
+            private String btMsg;
+
+            public workerThread(String msg) {
+                btMsg = msg;
+            }
+
+            public void run() {
+                sendBtMsg(btMsg);
+                while (!Thread.currentThread().isInterrupted()) {
+                    int bytesAvailable;
+                    boolean workDone = false;
+
+                    try {
+
+
+                        final InputStream mmInputStream;
+                        mmInputStream = mmSocket.getInputStream();
+                        bytesAvailable = mmInputStream.available();
+                        if (bytesAvailable > 0) {
+
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            Log.e("Aquarium recv bt", "bytes available");
+                            byte[] readBuffer = new byte[1024];
+                            mmInputStream.read(packetBytes);
+
+                            for (int i = 0; i < bytesAvailable; i++) {
+                                byte b = packetBytes[i];
+                                if (b == delimiter) {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    //The variable data now contains our full command
+                                    handler.post(new Runnable() {
+                                        public void run() {
+                                        }
+                                    });
+
+                                    workDone = true;
+                                    break;
+
+
+                                } else {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+
+                            if (workDone == true) {
+                                mmSocket.close();
+                                break;
+                            }
+
+                        }
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }
+        ;
+
+
+
+
+
+
+
+        fButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on temp button click
+
+                (new Thread(new workerThread("forward"))).start();
+
+            }
+        });
+
+        bButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on temp button click
+
+                (new Thread(new workerThread("backward"))).start();
+
+            }
+        });
+
+        lButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on temp button click
+
+                (new Thread(new workerThread("left"))).start();
+
+            }
+        });
+
+        rButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on temp button click
+
+                (new Thread(new workerThread("right"))).start();
+
+            }
+        });
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
